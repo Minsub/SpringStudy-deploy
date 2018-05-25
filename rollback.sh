b9b6f8526d361d0eb32e07886768fe25a97e63c6
gray.ji <gray.ji@kakaocorp.com>
2018-05-25 17:12:58 +0900

    add rollback

---
+++ rollback.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PHASE=$1
+DEPLOY_GROUP=$2
+BASE_DIR="${BASH_SOURCE%/*}"
+SCRIPT_FILENAME=`basename $0`
+
+PLAYBOOK_EXTRA_VARS="phase=$PHASE base_dir=$BASE_DIR deploy_group=$DEPLOY_GROUP"
+PLAYBOOK_FILE=${BASE_DIR}/rollback.yml
+
+ansible-playbook -vvvv --ask-become-pass \
+  -i ${BASE_DIR}/hosts/$PHASE \
+  -e "$PLAYBOOK_EXTRA_VARS" \
+  $PLAYBOOK_FILE
+
