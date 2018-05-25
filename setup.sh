2c25fa9bf544031768d1a80435cb3d68a6fa4493
macmini <macmini@macmini.local>
2018-05-25 12:20:36 +0900

    fix setup

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ PHASE=$1
 BASE_DIR="${BASH_SOURCE%/*}"
 SCRIPT_FILENAME=`basename $0`
 
+PLAYBOOK_EXTRA_VARS="base_dir=$BASE_DIR"
 PLAYBOOK_FILE=${BASE_DIR}/setup.yml
 
-# ansible-playbook -vvvv \
-# 	# --forks=20 \
-# 	# --private-key=${BASE_DIR}/files/ssh/id_rsa \
-# 	-i ${BASE_DIR}/hosts/$PHASE \
-# 	# -e "$PLAYBOOK_EXTRA_VARS" \
-# 	$PLAYBOOK_FILE
+ansible-playbook -vvvv --ask-become-pass \
+  -i ${BASE_DIR}/hosts/$PHASE \
+  -e "$PLAYBOOK_EXTRA_VARS" \
+  $PLAYBOOK_FILE
+
