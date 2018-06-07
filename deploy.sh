caf025ce40643f47a92df1cae82dac092c559b2a
macmini <macmini@macmini.local>
2018-06-07 15:31:20 +0900

    remove ask password in deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ SCRIPT_FILENAME=`basename $0`
 PLAYBOOK_EXTRA_VARS="phase=$PHASE base_dir=$BASE_DIR deploy_group=$DEPLOY_GROUP"
 PLAYBOOK_FILE=${BASE_DIR}/deploy.yml
 
-ansible-playbook -vvvv --ask-become-pass \
+ansible-playbook -vvvv \
   -i ${BASE_DIR}/hosts/$PHASE \
   -e "$PLAYBOOK_EXTRA_VARS" \
   $PLAYBOOK_FILE
